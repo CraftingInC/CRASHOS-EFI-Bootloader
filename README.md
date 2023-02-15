507f3fede653d903e2ff993c02aa52b513aa0276
CraftingInC <EpicVoxel@protonmail.com>
2023-02-14 22:57:02 -0500

    Finished the EFI Bootloader + GDT

--- README.md
+++ README.md
@@ -4,14 +4,20 @@
     This is the source code to the EFI Bootloader youtube series.  
 	**YOUTUBE :** https://www.youtube.com/watch?v=4CxOYxCjKDM&list=PLwH94sFU_ljN4ENYWT14e1oLRUKwAZVtp  
 	
-Use this link to grab the code needed to create the drive.hdd file.  
-https://github.com/CraftingInC/UEFI-GPT-image-creator  
+    Use this link to grab the code needed to create the drive.hdd file.  
+    https://github.com/CraftingInC/UEFI-GPT-image-creator  
+  
+  
+  
+**STATUS**  
+    This code is setup for the loader / kernel to begin it's development. RSDP has been added to access hardware. The GDT is called from within the EFI file itself, AFTER the Exit Boot Services has been called. Possible youtube video series for the loader / kernel in the future.  
+  
   
   
 **WARNING**  
     I take no responsibility of what you do with this code. You use the code at your own risk. You have been warned !  
 	
-CURRENT PROGRESS
+
 ![Current Progress](progress.png)  
 	
 	
--- efimain.c
+++ efimain.c
@@ -1,4 +1,6 @@
 
+// GITHUB : https://github.com/CraftingInC/CRASHOS-EFI-Bootloader
+
 #include "efi.h"
 #include "efilib.h"
 
@@ -36,6 +38,85 @@ UINT64 CompareGuid(EFI_GUID* guid1, EFI_GUID* guid2)  // Might need fixing, I am
 	r |= g1[3] - g2[3];
 
 	return r;
+}
+
+struct GDTEntry
+{
+    UINT16    limit_low;
+    UINT16    base_low;
+    UINT8     base_mid;
+    UINT8     access;
+    UINT8     granularity;
+    UINT8     base_high;
+} __attribute__((__packed__));
+
+struct GDTRegister
+{
+    UINT16    limit;
+    UINT64    base;
+} __attribute__((__packed__));
+
+static struct {
+    struct GDTEntry entry[5];
+} __attribute__((__packed__)) gdt;
+
+static struct GDTRegister GDTReg;
+
+void gdt_init()
+{
+    gdt.entry[0].limit_low = 0;
+    gdt.entry[0].base_low = 0;
+    gdt.entry[0].base_mid = 0;
+    gdt.entry[0].access = 0;
+    gdt.entry[0].granularity = 0;
+    gdt.entry[0].base_high = 0;
+
+    gdt.entry[1].limit_low = 0;
+    gdt.entry[1].base_low = 0;
+    gdt.entry[1].base_mid = 0;
+    gdt.entry[1].access = 0b10011010;
+    gdt.entry[1].granularity = 0b00100000;
+    gdt.entry[1].base_high = 0;
+
+    gdt.entry[2].limit_low = 0;
+    gdt.entry[2].base_low = 0;
+    gdt.entry[2].base_mid = 0;
+    gdt.entry[2].access = 0b10010010;
+    gdt.entry[2].granularity = 0;
+    gdt.entry[2].base_high = 0;
+
+    gdt.entry[3].limit_low = 0;
+    gdt.entry[3].base_low = 0;
+    gdt.entry[3].base_mid = 0;
+    gdt.entry[3].access = 0b11111010;
+    gdt.entry[3].granularity = 0b00100000;
+    gdt.entry[3].base_high = 0;
+
+    gdt.entry[4].limit_low = 0;
+    gdt.entry[4].base_low = 0;
+    gdt.entry[4].base_mid = 0;
+    gdt.entry[4].access = 0b11110010;
+    gdt.entry[4].granularity = 0;
+    gdt.entry[4].base_high = 0;
+
+    GDTReg.limit = (UINT16) sizeof(gdt) - 1;
+    GDTReg.base = (UINT64) &gdt;
+
+    __asm__ __volatile__("lgdt %0" :: "m"(GDTReg) : "memory");
+
+	__asm__ __volatile__(
+        "push %0\n\t"
+        "push $1f\n\t"
+        "lretq\n\t"
+        "1:\n\t"
+        "mov %1, %%ds\n\t"
+        "mov %1, %%es\n\t"
+        "mov %1, %%fs\n\t"
+        "mov %1, %%gs\n\t"
+        "mov %1, %%ss\n\t"
+        :: "rmi"(0x08), "rm"(0x10)
+        : "memory"
+    );
 }
 
 EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *ST)
@@ -84,7 +165,7 @@ EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *ST)
 	conOut->SetAttribute(conOut, EFI_GREEN);
     conOut->OutputString(conOut, u"Grabbing MMAP ...\r\n");
 
-// NOTE : Do NOT use any conOut from this point on. It messes up the MMap.
+    // NOTE : Do NOT use any conOut from this point on. It messes up the MMap.
     UINTN                   MemoryMapSize = 0;
 	EFI_MEMORY_DESCRIPTOR*  MemoryMap = NULL;
 	UINTN                   MapKey;
@@ -113,7 +194,9 @@ EFI_STATUS efi_main(EFI_HANDLE IH, EFI_SYSTEM_TABLE *ST)
 	
 	// Exit EFI here.
 	SystemTable->BootServices->ExitBootServices(ImageHandle, MapKey);
-	
+
+	gdt_init();
+
 	LoaderFileBin(&bi);
 
 	conOut->SetAttribute(conOut, EFI_RED);
